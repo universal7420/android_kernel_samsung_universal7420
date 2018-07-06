bd9ce720326e7a3d1121260c30bec6a8eda6e176
Daniel Rosenberg <drosen@google.com>
2018-07-06 16:24:27 -0700

    ANDROID: sdcardfs: Add option to drop unused dentries
    This adds the nocache mount option, which will cause sdcardfs to always
    drop dentries that are not in use, preventing cached entries from
    holding on to lower dentries, which could  cause strange behavior when
    bypassing the sdcardfs layer and directly changing the lower fs.
    
    Change-Id: I70268584a20b989ae8cfdd278a2e4fa1605217fb
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    Signed-off-by: Lunarixus <Lunarixus@raphielgang.org>

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -33,6 +33,8 @@ enum {
 	Opt_userid,
 	Opt_reserved_mb,
 	Opt_gid_derivation,
+	Opt_default_normal,
+	Opt_nocache,
 	Opt_err,
 };
 
@@ -46,6 +48,7 @@ static const match_table_t sdcardfs_tokens = {
 	{Opt_multiuser, "multiuser"},
 	{Opt_gid_derivation, "derive_gid"},
 	{Opt_reserved_mb, "reserved_mb=%u"},
+	{Opt_nocache, "nocache"},
 	{Opt_err, NULL}
 };
 
@@ -66,12 +69,10 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 	vfsopts->gid = 0;
 	/* by default, 0MB is reserved */
 	opts->reserved_mb = 0;
-#if ANDROID_VERSION > 80000
 	/* by default, gid derivation is off */
 	opts->gid_derivation = false;
-#else
-	opts->gid_derivation = true;
-#endif
+	opts->default_normal = false;
+	opts->nocache = false;
 
 	*debug = 0;
 
@@ -125,6 +126,12 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 			break;
 		case Opt_gid_derivation:
 			opts->gid_derivation = true;
+			break;
+		case Opt_default_normal:
+			opts->default_normal = true;
+			break;
+		case Opt_nocache:
+			opts->nocache = true;
 			break;
 		/* unknown option */
 		default:
