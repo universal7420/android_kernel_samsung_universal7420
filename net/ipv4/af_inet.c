ca1b9226a54e0336b8a2f126435affddf9bc496b
ananjaser1211 <ananjaser@gmail.com>
2019-09-26 23:14:01 +0400

    net: disable knox_ncm

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -446,6 +446,7 @@ out_rcu_unlock:
 	goto out;
 }
 
+#ifdef CONFIG_KNOX_NCM
 /** The function is used to check if the ncm feature is enabled or not; if enabled then collect the socket meta-data information; **/
 static void knox_collect_metadata(struct socket *sock) {
     if(check_ncm_flag()) {
@@ -504,6 +505,7 @@ static void knox_collect_metadata(struct socket *sock) {
         }
     }
 }
+#endif
 
 /*
  *	The peer socket should always be NULL (or else). When we call this
@@ -536,7 +538,9 @@ int inet_release(struct socket *sock)
 		if (sock_flag(sk, SOCK_LINGER) &&
 		    !(current->flags & PF_EXITING))
 			timeout = sk->sk_lingertime;
+#ifdef CONFIG_KNOX_NCM
 		knox_collect_metadata(sock);
+#endif
 		sock->sk = NULL;
 		sk->sk_prot->close(sk, timeout);
 	}
